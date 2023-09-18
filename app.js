38c4795e7b42cb4a4b8bc3245d90480c062af73f
Rahul Khanal <rahul39khanal@gmail.com>
2023-09-18 21:53:29 +0545

    server connection established

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+//declaration
+const PORT = process.env.PORT || 8282;
+
+//middlewares
+app.use(bodyParser.json());
+
+//server
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  else {
+    console.log(`connected your app in PORT:${PORT}`);
+  }
+});
